9f25079fb21ef4392b2a904c6cd746f65d4049e0
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2026-04-22 16:50:32 +0900

    chore: 🔧 Update config

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+// #include <atcoder/fenwicktree>
+// #include <atcoder/segtree>
+// #include <atcoder/lazysegtree>
+// #include <atcoder/string>
+// #include <atcoder/math>
+// #include <atcoder/convolution>
+// #include <atcoder/modint>
+// #include <atcoder/dsu>
+// #include <atcoder/maxflow>
+// #include <atcoder/mincostflow>
+// #include <atcoder/scc>
+// #include <atcoder/twosat>
+// #include <boost/multiprecision/cpp_int.hpp>
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep3(i, m, n) for (int i = m; i < (int)(n); i++)
+#define all(x) (x).begin(), (x).end()
+using namespace std;
+// using namespace atcoder;
+// using namespace boost::multiprecision;
+typedef long long ll;
+// typedef modint1000000007 mint;
+// typedef modint998244353 mint;
+
+int main(void)
+{
+    cout << "Hello, World!" << endl;
+    return 0;
+}
