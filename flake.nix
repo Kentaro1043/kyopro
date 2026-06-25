a708ed102e0a5b80c8eaeebc76a8ec092d3dd8a6
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2026-06-25 23:47:51 +0900

    Format flake.nix

--- flake.nix
+++ flake.nix
@@ -5,33 +5,28 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs =
-    { nixpkgs, ... }:
-    let
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "aarch64-darwin"
-      ];
-      forAllSystems = nixpkgs.lib.genAttrs systems;
-    in
-    {
-      devShells = forAllSystems (
-        system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              ac-library
-              boost
-              clang-tools
-              gcc
-              llvmPackages.lldb
-            ];
-          };
-        }
-      );
-    };
+  outputs = {nixpkgs, ...}: let
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+    devShells = forAllSystems (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            ac-library
+            boost
+            clang-tools
+            gcc
+            llvmPackages.lldb
+          ];
+        };
+      }
+    );
+  };
 }
